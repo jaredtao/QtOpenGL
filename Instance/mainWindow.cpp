5781ad85e24fd73ad4193dd546d113d3c91fadb8
jared <jared2020@163.com>
2023-04-25 09:08:10 +0800

    add logger

--- Instance/mainWindow.cpp
+++ Instance/mainWindow.cpp
@@ -34,88 +34,131 @@ void main(void)
     fragColor = outColor0;
 }
 )";
-MainWindow::MainWindow(QWidget *parent) : Super(parent) { }
+MainWindow::MainWindow(QWidget* parent)
+	: Super(parent)
+{
+}
 
-MainWindow::~MainWindow() { }
+MainWindow::~MainWindow()
+{
+	makeCurrent();
+#ifdef _DEBUG
+	if (mLogger)
+	{
+
+		mLogger->stopLogging();
+		delete mLogger;
+		mLogger = nullptr;
+	}
+#endif
+	doneCurrent();
+}
 
 void MainWindow::initializeGL()
 {
-    initializeOpenGLFunctions();
-    glClearColor(0.2, 0.3, 0.4, 1);
+	initializeOpenGLFunctions();
+#ifdef _DEBUG
+	mLogger = new QOpenGLDebugLogger(this);
+	mLogger->initialize();
+	connect(mLogger, &QOpenGLDebugLogger::messageLogged, this, [this](const QOpenGLDebugMessage& msg) { qWarning() << msg.message(); });
+	mLogger->startLogging();
+#endif
 
-    mProgram = new QOpenGLShaderProgram(this);
-    if (!mProgram->addShaderFromSourceCode(QOpenGLShader::Vertex, sVertexShader)) {
-        qWarning() << mProgram->log();
-        return;
-    }
-    if (!mProgram->addShaderFromSourceCode(QOpenGLShader::Fragment, sFragmentShader)) {
-        qWarning() << mProgram->log();
-        return;
-    }
-    if (!mProgram->link()) {
-        qWarning() << mProgram->log();
-        return;
-    }
-    initVertices();
-    glGenBuffers(1, &mVBO);
-    glGenVertexArrays(1, &mVAO);
+	glClearColor(0.2, 0.3, 0.4, 1);
 
-    const uint32_t vertexLocation = 0;
-    glBindVertexArray(mVAO);
-    {
-        glBindBuffer(GL_ARRAY_BUFFER, mVBO);
-        glBufferData(mVBO, sizeof(mVertices), mVertices.constData(), GL_STATIC_DRAW);
-        glEnableVertexAttribArray(vertexLocation);
-        glVertexAttribPointer(vertexLocation, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void *)0);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glVertexAttribDivisor(vertexLocation, 1);
-    }
-    glBindVertexArray(0);
+	mProgram = new QOpenGLShaderProgram(this);
+	if (!mProgram->addShaderFromSourceCode(QOpenGLShader::Vertex, sVertexShader))
+	{
+		qWarning() << mProgram->log();
+		return;
+	}
+	if (!mProgram->addShaderFromSourceCode(QOpenGLShader::Fragment, sFragmentShader))
+	{
+		qWarning() << mProgram->log();
+		return;
+	}
+	if (!mProgram->link())
+	{
+		qWarning() << mProgram->log();
+		return;
+	}
+	initVertices();
+	glGenBuffers(1, &mVBO);
+	glGenVertexArrays(1, &mVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(QVector3D) * mVertices.size(), mVertices.constData(), GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	const uint32_t vertexLocation = 0;
+	glBindVertexArray(mVAO);
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+		glEnableVertexAttribArray(vertexLocation);
+		glVertexAttribPointer(vertexLocation, 3, GL_FLOAT, GL_FALSE, sizeof(QVector3D), (void*)0);
+
+		glVertexAttribDivisor(vertexLocation, 1);
+	}
+	glBindVertexArray(0);
+
+	startTimer(1000 / 60);
 }
 
 void MainWindow::resizeGL(int w, int h)
 {
-    glViewport(0, 0, w, h);
-    Super::resizeGL(w, h);
+	glViewport(0, 0, w, h);
+	Super::resizeGL(w, h);
 }
 
 void MainWindow::paintGL()
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glClearColor(0.2, 0.3, 0.4, 1);
-    if (!mProgram->bind()) {
-        qWarning() << mProgram->log();
-        return;
-    }
-    mProgram->setUniformValue("outColor0", mColor);
-    glBindVertexArray(mVAO);
-    {
-        glDrawArraysInstanced(GL_TRIANGLES, 0, 4, mCount);
-    }
-    glBindVertexArray(0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.2, 0.3, 0.4, 1);
+	if (!mProgram->bind())
+	{
+		qWarning() << mProgram->log();
+		return;
+	}
+	mProgram->setUniformValue("outColor0", mColor);
+	glBindVertexArray(mVAO);
+	{
+		//		glDrawArrays(GL_TRIANGLE_FAN, 0, mVertices.size());
+		glDrawArraysInstanced(GL_TRIANGLE_FAN, 0, mVertices.size(), mCount);
+	}
+	glBindVertexArray(0);
+}
+
+void MainWindow::timerEvent(QTimerEvent* e)
+{
+	if (isVisible())
+	{
+		update();
+	}
 }
 
 void MainWindow::initVertices()
 {
-    mCount = 1024;
-    qreal w = 0.003;
-    qreal h = 0.002;
-    qreal x = 0.001;
-    qreal y = 0.001;
-    qreal xOffset = 0.001;
-    qreal yOffset = 0.001;
-    mVertices.clear();
-    for (int i = 0; i < mCount; i++) {
-        mVertices << QVector3D(x, y, 0.0);
-        mVertices << QVector3D(x + w, y, 0.0);
-        mVertices << QVector3D(x + w, y + h, 0.0);
-        mVertices << QVector3D(x, y + h, 0.0);
-        x += xOffset;
-        if (x + w >= 1.0) {
-            x = 0.001;
-            y += yOffset;
-        }
-    }
+	mCount		  = 1;
+	qreal x		  = 0.01;
+	qreal y		  = 0.01;
+	qreal w		  = 0.03;
+	qreal h		  = 0.02;
+	qreal xOffset = 0.01;
+	qreal yOffset = 0.01;
+	mVertices.clear();
+	for (int i = 0; i < mCount; i++)
+	{
+		mVertices << QVector3D(x, y, 0.0);
+		mVertices << QVector3D(x + w, y, 0.0);
+		mVertices << QVector3D(x + w, y + h, 0.0);
+		mVertices << QVector3D(x, y + h, 0.0);
+		x += xOffset;
+		if (x + w >= 1.0)
+		{
+			x = 0.01;
+			y += yOffset;
+		}
+	}
 }
 
 void MainWindow::updateVertices() { }
--- Instance/mainWindow.h
+++ Instance/mainWindow.h
@@ -1,32 +1,39 @@
 #include "qnamespace.h"
+#include <QOpenGLDebugLogger>
 #include <QOpenGLFunctions_3_3_Core>
 #include <QOpenGLShaderProgram>
 #include <QOpenGLWidget>
 #include <QVector3D>
 #include <QVector>
-class MainWindow : public QOpenGLWidget, public QOpenGLFunctions_3_3_Core
+class MainWindow
+	: public QOpenGLWidget
+	, public QOpenGLFunctions_3_3_Core
 {
-    Q_OBJECT
-    using Super = QOpenGLWidget;
+	Q_OBJECT
+	using Super = QOpenGLWidget;
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-    // QOpenGLWidget interface
+	MainWindow(QWidget* parent = nullptr);
+	~MainWindow();
+
 protected:
-    virtual void initializeGL() override;
-    virtual void resizeGL(int w, int h) override;
-    virtual void paintGL() override;
+	virtual void initializeGL() override;
+	virtual void resizeGL(int w, int h) override;
+	virtual void paintGL() override;
+	virtual void timerEvent(QTimerEvent* e) override;
 
 private:
-    void initVertices();
-    void updateVertices();
+	void initVertices();
+	void updateVertices();
 
 private:
-    QOpenGLShaderProgram *mProgram;
-    GLuint mVBO = 0;
-    GLuint mVAO = 0;
-    QColor mColor = Qt::red;
-    QVector<QVector3D> mVertices;
-    uint32_t mCount = 0;
+#ifdef _DEBUG
+	QOpenGLDebugLogger* mLogger = nullptr;
+#endif
+	QOpenGLShaderProgram* mProgram = nullptr;
+	GLuint				  mVBO	   = 0;
+	GLuint				  mVAO	   = 0;
+	QColor				  mColor   = Qt::red;
+	QVector<QVector3D>	  mVertices;
+	uint32_t			  mCount = 0;
 };
